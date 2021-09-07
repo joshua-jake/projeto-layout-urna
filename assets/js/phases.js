b7493f69cedfe175b625b928da4ab194d34516f4
Jesuan Goalhath <jesuanprincdd@gmail.com>
2021-09-06 22:12:26 -0300

    Aplicando Javascript no Layout da Urna 1 parte

---
+++ assets/js/phases.js
@@ -0,0 +1,45 @@
+// Criando um JSON 
+
+let phases = [
+    {
+        titles: 'VEREADOR',
+        numbers: 7,
+        candidates: [
+            {
+                number: 97458,
+                name: 'Joshua',
+                broken: 'JOSH',
+                photos:[
+                    {url:'14544.jpg', subtitle: 'councilor', small: false}
+                ]
+
+            },
+            {
+                number: 77547,
+                name: 'Israel',
+                broken: 'ISRA',
+                PHOTOS: [
+                    {url:'77547', subtitle: 'councilor',}
+                ]
+            },
+
+        ]
+    },
+    {
+        subtitle: 'PREFECT',
+        namber: 2,
+        candidates: [
+            {
+                number: 77,
+                name: 'Jesus',
+                broken: 'JESU',
+                vice: 'Jes',
+                photos: [
+                    {url:'10.jpg', subtitle: 'Prefect'},
+                    {url:'10_7.jpg', subtitle: 'Vice-Prefect', small: true}
+                ]
+            },
+        ]
+    },
+
+];
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Selecting screen items with Java Script - control the items
+
+// selected the first item --- selecionando o primeiro item 
+
+let yourVoteFor = document.querySelector('.d-1-1 span');
+let role = document.querySelector('.d-1-2 span');
+let description = document.querySelector('.d-1-4');
+let warning = document.querySelector('.d-2');
+let side = document.querySelector('.d-1-right');
+let numbers = document.querySelector('.d-1-3');
+
+
+
+
+
+
